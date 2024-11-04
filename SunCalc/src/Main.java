0d5585572b022eb7a8e471eb67f56ec52c46861c
Skeledan <121405337+TheRealSkeledan@users.noreply.github.com>
2024-11-04 16:43:43 -0500

    Update Main.java

--- SunCalc/src/Main.java
+++ SunCalc/src/Main.java
@@ -1,10 +1,24 @@
+import java.awt.Desktop;
+import java.net.URI;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("SunCalc\n\nEnter your terms in the format\nN1 N2 F\nOr\nN1 F\nFor a list of functions, view the following list https://bit.ly/SunCalcFunctions");
+        System.out.println("SunCalc\n\nEnter your terms in the format\nN1 N2 F\nOr\nN1 F\nFor a list of functions, view the following list");
+        try {
+
+            if (Desktop.isDesktopSupported()) {
+                Desktop desktop = Desktop.getDesktop();
+                URI uri = new URI("https://bit.ly/SunCalcFunctions");
+                desktop.browse(uri);
+            } else {
+                System.out.println("Desktop is not supported on this system.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         String terms = scan.nextLine();
 
         Storing store = new Storing((terms));
