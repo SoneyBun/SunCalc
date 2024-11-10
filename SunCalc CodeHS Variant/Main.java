8e6e32eaff85da9cc0d564ae6013c26ff43e3419
Sunny <addrosity.yt@gmail.com>
2024-11-10 10:05:28 -0500

    Create Main.java

---
+++ SunCalc CodeHS Variant/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("This is the public testing version for SunCalc. Expect Bugs and older versions due to CodeHS limitations.\nSunCalc\n\nEnter your terms in the format\nN1 N2 F\nOr\nN1 F\nFor a list of functions, view the following list https://bit.ly/SunCalcFunctions");
+        String terms = scan.nextLine();
+
+        Storing store = new Storing((terms));
+        Operation answer = new Operation(store.getNum1(), store.getNum2(), store.getFunction());
+
+        System.out.println(answer);
+    }
+}
