4f5845c55f0a6cba1901d0a6e874189ad3fcc4f4
Sunny <addrosity.yt@gmail.com>
2024-11-16 19:08:27 -0500

    Update Operation.java
    New operators, replaced some portions with more advanced. suppressed warnings. added many more ways to use certain operators.

--- SunCalc/src/Operation.java
+++ SunCalc/src/Operation.java
@@ -1,5 +1,6 @@
 import java.lang.Math;
 import java.math.BigInteger;
+@SuppressWarnings("SpellCheckingInspection")
 
 public class Operation {
     private final double n1, n2;
@@ -22,14 +23,12 @@ public class Operation {
 
     public String toString() {
         switch (fn.toLowerCase()) {
-            case "+" :
-                if((n1 == 9 && n2 == 10) || (n1 == 10 && n2 == 9)) {
-                    return(n1 + " + " + n2 + " = " + (n1 + n2) + "\n\n\n\n\n9 + 10 = 21");
-                }
-                return (n1 + " + " + n2 + " = " + (n1 + n2));
-            case "-" : return (n1 + " - " + n2 + " = " + (n1 - n2));
-            case "*" : return (n1 + " * " + n2 + " = " + (n1 * n2));
-            case "/" :
+            case "+", "add", "plus" : return (n1 + " + " + n2 + " = " + (n1 + n2));
+            case "++", "increment" : return (n1 + "++ = " + (n1 + 1));
+            case "-", "subtract", "minus" : return (n1 + " - " + n2 + " = " + (n1 - n2));
+            case "--", "decrement" : return (n1 + "-- = " + (n1 - 1));
+            case "*", "multiply", "times" : return (n1 + " * " + n2 + " = " + (n1 * n2));
+            case "/", "divide" :
                 if(n2 != 0) {
                     return (n1 + " / " + n2 + " = " + (n1 / n2));
                 }
@@ -43,13 +42,13 @@ public class Operation {
             case ">=" : return ("" + (n1 >= n2));
             case "<" : return ("" + (n1 < n2));
             case "<=" : return ("" + (n1 <= n2));
-            case "=", "==" : return("" + (n1 == n2));
+            case "=", "==" : return ("" + (n1 == n2));
             case "!=" : return ("" + (n1 != n2));
-            case "max" : return ("" + Math.max(n1, n2));
-            case "min" : return ("" + Math.min(n1, n2));
-            case "%" : return (n1 + " % " + n2 + " = " + (n1 % n2));
+            case "max", "maximum" : return ("" + Math.max(n1, n2));
+            case "min", "minimum" : return ("" + Math.min(n1, n2));
+            case "%", "modulus", "mod" : return (n1 + " % " + n2 + " = " + (n1 % n2));
             case "||" : return ("|" + n1 + "| = " + Math.abs(n1));
-            case "!" :
+            case "!", "factorial" :
                 BigInteger factorial = BigInteger.ONE;
                 BigInteger factorialTruncatedN1 = BigInteger.ONE;
                 BigInteger factorialRoundedN1 = BigInteger.ONE;
@@ -63,7 +62,7 @@ public class Operation {
                     }
                     else {
                         int truncatedN1 = (int) (n1);
-                        int roundedN1 = (int) (n1 + .5);
+                        int roundedN1 = (int) Math.round(n1);
 
                         for(int i = 1; i <= truncatedN1; i++) {
                             factorialTruncatedN1 = factorialTruncatedN1.multiply(BigInteger.valueOf(i));
@@ -77,7 +76,7 @@ public class Operation {
                             return (factorialOfADouble + "\nThe factorial of " + n1 + " truncated is \n" + truncatedN1 + "! = " + factorialTruncatedN1 + "\nThe factorial of " + n1 + " rounded is \n" + roundedN1 + "! = " + factorialRoundedN1);
                         }
                         else {
-                            return (factorialOfADouble + "\nThe factorial of " + n1 + " truncated/rounded is \n" + truncatedN1 + "! = " + truncatedN1);
+                            return (factorialOfADouble + "\nThe factorial of " + n1 + " truncated/rounded is \n" + truncatedN1 + "! = " + factorialTruncatedN1);
                         }
                     }
                 }
@@ -90,7 +89,7 @@ public class Operation {
                     }
 
                     int positiveTruncatedN1 = (int) (n1 * -1);
-                    int positiveRoundedN1 = (int) ((n1 * -1) + .5);
+                    int positiveRoundedN1 = (int) Math.round(n1 * -1);
 
                     BigInteger factorialPositiveTruncatedN1 = BigInteger.ONE;
                     BigInteger factorialPositiveRoundedN1 = BigInteger.ONE;
@@ -110,21 +109,22 @@ public class Operation {
                         return (factorialOfADouble + "\n" + undefinedFactorialOfANegative + "\nThe factorial of " + n1 + " positive and truncated/rounded is \n" + positiveTruncatedN1 + "! = " + factorialPositiveTruncatedN1);
                     }
                 }
-            case "^" : return (n1 + "^" + n2 + " = " + Math.pow(n1, n2));
-            case "rt" : return (n1 + "^(1.0/" + n2 + ") = " + Math.pow(n1, (1 / n2)));
+            case "^", "exp" : return (n1 + "^" + n2 + " = " + Math.pow(n1, n2));
+            case "rt", "root" : return (n1 + "^(1.0/" + n2 + ") = " + Math.pow(n1, (1 / n2)));
             case "pi" : return (n1 + "pi = " + (n1 * Math.PI));
             case "e" : return (n1 + "e = " + (n1 * Math.E));
-            case "sin" : return ("sin(" + n1 + ") = " + Math.sin(n1));
-            case "asin" : return ("asin(" + n1 + ") = " + Math.asin(n1));
+            case "rnd", "round" : return (n1 + " rounded is " + Math.round(n1));
+            case "sin", "sine" : return ("sin(" + n1 + ") = " + Math.sin(n1));
+            case "asin", "arcsine", "arcsin" : return ("asin(" + n1 + ") = " + Math.asin(n1));
             case "sinh" : return ("sinh(" + n1 + ") = " + Math.sinh(n1));
-            case "cos" : return ("cos(" + n1 + ") = " + Math.cos(n1));
-            case "acos" : return ("acos(" + n1 + ") = " + Math.acos(n1));
+            case "cos", "cosine" : return ("cos(" + n1 + ") = " + Math.cos(n1));
+            case "acos", "arccosine", "arccos" : return ("acos(" + n1 + ") = " + Math.acos(n1));
             case "cosh" : return ("cosh(" + n1 + ") = " + Math.cosh(n1));
-            case "tan" : return ("tan(" + n1 + ") = " + Math.tan(n1));
-            case "atan" : return ("atan(" + n1 + ") = " + Math.atan(n1));
+            case "tan", "tangent" : return ("tan(" + n1 + ") = " + Math.tan(n1));
+            case "atan", "arctangent", "arctan" : return ("atan(" + n1 + ") = " + Math.atan(n1));
             case "tanh" : return ("tanh(" + n1 + ") = " + Math.tanh(n1));
             case "ln" : return ("ln(" + n1 + ") = " + Math.log(n1));
-            case "log10" : return("log10(" + n1 + ") = " + Math.log10(n1));
+            case "log10" : return ("log10(" + n1 + ") = " + Math.log10(n1));
         }
         return ("" + unknownFunction);
     }
