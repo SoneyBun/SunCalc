d19814c404de20f479ff051e8e441de426a059ba
Sunny <addrosity.yt@gmail.com>
2024-11-20 07:37:23 -0500

    Update Main.java
    while condition to keep using SunCalc

--- SunCalc/src/Main.java
+++ SunCalc/src/Main.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        String terms = "";
+        Storing store;
+        Operation answer;
 
-        System.out.println("SunCalc\n\nSunCalc is a calculator for math, not to get information about sun and moon times.\nEnter your terms in the format\nN1 N2 F\nOr\nN1 F\nFor a list of functions, view the following list");
         try {
-
             if(Desktop.isDesktopSupported()) {
                 Desktop desktop = Desktop.getDesktop();
                 URI uri = new URI("https://bit.ly/SunCalcFunctions");
@@ -21,11 +22,21 @@ public class Main {
         catch(Exception e) {
             e.printStackTrace();
         }
-        String terms = scan.nextLine();
 
-        Storing store = new Storing((terms));
-        Operation answer = new Operation(store.getNum1(), store.getNum2(), store.getFunction());
+        System.out.println("SunCalc\n\nSunCalc is a calculator for math, not to get information about sun and moon times.\nEnter your terms in the format\nN1 N2 F\nOr\nN1 F\nFor a list of functions, view the following list: https://bit.ly/SunCalcFunctions\nType esc to quit");
 
-        System.out.println(answer);
+        while(!(terms.equalsIgnoreCase("esc"))) {
+            terms = scan.nextLine();
+
+            if(terms.equalsIgnoreCase("esc")) {
+                System.out.println("Thank you for using SunCalc");
+                System.exit(0);
+            }
+
+            store = new Storing((terms));
+            answer = new Operation(store.getNum1(), store.getNum2(), store.getFunction());
+
+            System.out.println(answer);
+        }
     }
 }
