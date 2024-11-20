121be553dc99366913083dd045a1c8c70cad8d7f
Sunny <addrosity.yt@gmail.com>
2024-11-20 07:43:59 -0500

    Update Main.java
    adjusted to somewhat the newest version

--- SunCalc CodeHS Variant/Main.java
+++ SunCalc CodeHS Variant/Main.java
@@ -3,13 +3,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        String terms = "";
+        Storing store;
+        Operation answer;
 
-        System.out.println("This is the public testing version for SunCalc. Expect Bugs and older versions due to CodeHS limitations.\nSunCalc\n\nEnter your terms in the format\nN1 N2 F\nOr\nN1 F\nFor a list of functions, view the following list https://bit.ly/SunCalcFunctions");
-        String terms = scan.nextLine();
+        System.out.println("This is the public testing version for SunCalc. Expect Bugs and older versions due to CodeHS limitations.\nSunCalc\n\nEnter your terms in the format\nN1 N2 F\nOr\nN1 F\nFor a list of functions, view the following list https://bit.ly/SunCalcFunctions\nType esc to quit");
+        
+        while(!(terms.equalsIgnoreCase("esc"))) {
+            terms = scan.nextLine();
+            
+            if(terms.equalsIgnoreCase("esc")) {
+                System.out.println("Thank you for using SunCalc");
+                System.exit(0);
+            }
 
-        Storing store = new Storing((terms));
-        Operation answer = new Operation(store.getNum1(), store.getNum2(), store.getFunction());
+            store = new Storing((terms));
+            answer = new Operation(store.getNum1(), store.getNum2(), store.getFunction());
 
-        System.out.println(answer);
+            System.out.println(answer);
+        }
     }
 }
