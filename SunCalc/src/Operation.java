100128873915aa6265fd6fec1c50d07d2afc91ad
Sunny <addrosity.yt@gmail.com>
2024-11-25 09:19:29 -0500

    Update Operation.java

--- SunCalc/src/Operation.java
+++ SunCalc/src/Operation.java
@@ -17,6 +17,7 @@ public class Operation {
     private final Report undefinedDivideByZero = assignReport("Undefined - Dividing By Zero");
     private final Report undefinedFactorialOfANegative = assignReport("Undefined - Factorial Of A Negative");
     private final Report factorialOfADouble = assignReport("Error - Factorial Of A Double");
+    private final Report rootOfANegative = assignReport("Error - Root Of A Negative");
 
     public Report assignReport(String reason) {
         return new Report(reason);
@@ -158,9 +159,14 @@ public class Operation {
                 answer2 = 0;
                 return (n1 + "^" + n2 + " = " + answer);
             case "rt", "root" :
-                answer = Math.pow(n1, (1 / n2));
                 answer2 = 0;
-                return (n1 + "^(1.0/" + n2 + ") = " + answer);
+                if(n1 >= 0) {
+                    answer = Math.pow(n1, (1 / n2));
+                    return (n1 + "^(1.0/" + n2 + ") = " + answer);
+                }
+                else {
+                    return (rootOfANegative.toString());
+                }
             case "pi" :
                 answer = (n1 * Math.PI);
                 answer2 = 0;
@@ -213,7 +219,7 @@ public class Operation {
                 answer = Math.tanh(n1);
                 answer2 = 0;
                 return ("tanh(" + n1 + ") = " + answer);
-            case "ln" :
+            case "ln", "loge" :
                 answer = Math.log(n1);
                 answer2 = 0;
                 return ("ln(" + n1 + ") = " + answer);
@@ -222,7 +228,7 @@ public class Operation {
                 answer2 = 0;
                 return ("log10(" + n1 + ") = " + answer);
         }
-        return ("" + unknownFunction);
+        return (unknownFunction.toString());
     }
 
     public double getAnswer() {
